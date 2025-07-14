df04d5a8b6800b59c93caeb1d02a6808d02260f5
manideepbangaru <bmd994@gmail.com>
2025-07-14 23:48:57 +0530

    Replace google_search tool with a new function to retrieve the current time in the `tool_agent`. Introduce `get_current_time` function to format and return the current date and time.

--- 05_tool_agent/tool_agent/agent.py
+++ 05_tool_agent/tool_agent/agent.py
@@ -1,8 +1,17 @@
 import os
 from google.adk.agents import Agent
 from google.adk.tools import google_search
+from datetime import datetime
 
 AGENT_MODEL = os.getenv("GEMINI_MODEL")
+
+def get_current_time() -> dict:
+    """
+    Get the current time in the format of dd/mm/yyyy hh:mm:ss
+    """
+    return {
+        "Current Time": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        }
 
 root_agent = Agent(
     name="tool_agent",
@@ -10,5 +19,6 @@ root_agent = Agent(
     description="Agent to answer questions using Google Search.",
     instruction="""Your are an helpful agent that have access to below tools: 
                    - google_search: to search the web for information""",
-    tools=[google_search],
+    # tools = [google_search],
+    tools = [get_current_time]
 )
