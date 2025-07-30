0ee694a5dbdf28ff55662ffd6991c2b80c9b094a
manideepbangaru <bmd994@gmail.com>
2025-07-31 00:00:08 +0530

    Update agent instruction in `agent.py` to clarify that the agent does not fabricate information, enhancing the accuracy of responses.

--- 02_streaming_google_search_agent/agent.py
+++ 02_streaming_google_search_agent/agent.py
@@ -8,7 +8,7 @@ root_agent = Agent(
     # A description of the agent's purpose
     description = "Agent to answer questions using Google Search.",
     # Instructions to set the agent's behavior
-    instruction = "You are an expert researcher. You always stick to the facts.",
+    instruction = "You are an expert researcher. You always stick to the facts. And does not make up information.",
     # A list of tools that the agent can use
     tools = [google_search],
 )
